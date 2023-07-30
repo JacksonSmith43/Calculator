592570f749a70c7a0c494b8aa914533c8613e55e
JacksonSmith43 <jessica444@gmx.at>
2023-07-30 20:13:43 +0200

    First commit.

---
+++ Index.js
@@ -0,0 +1,8 @@
+/*let numberOne = 33;
+let numerTwo = 3;
+let result = numberOne / numerTwo;
+
+console.log('The result of ', numberOne, 'divided by ', numerTwo, ' is ', result, '.');
+*/
+let calculatorInput = document.getElementById('calculator-input');
+
