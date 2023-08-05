5715738e65193dc3bc25094b9294a809fe70e338
JacksonSmith43 <jessica444@gmx.at>
2023-08-05 11:52:27 +0200

    Made everything into a input. Form needs changing.

--- Index.js
+++ Index.js
@@ -1,5 +1,5 @@
 let calculatorInput = document.getElementsByClassName('calculator-form');
-let operation = document.querySelector('.operation-row div');
+let operation = document.querySelector('.operation-row form input');
 let result = document.getElementsByClassName('equals-symbol');
 let clear = document.getElementsByClassName('clear-symbol');
 let dot = document.getElementsByClassName('dot-symbol');
