352ad7a669737b25ea3cf00ed82610da62523c14
JacksonSmith43 <jessica444@gmx.at>
2023-08-15 20:31:47 +0200

    Changed and added functions in the JS file.

--- Index.js
+++ Index.js
@@ -1,8 +1,8 @@
 (function () {
 
-    let calculatorInput = document.querySelector('input[name="display"]');    let operation = document.querySelector('#operation-row');
-    let result = document.getElementsByClassName('equals-symbol');
-    let clear = document.getElementsByClassName('clear-symbol');
+    let calculatorInput = document.querySelector('input[name="display"]'); let operation = document.querySelector('#operation-row');
+    let equal = document.getElementsByClassName('equals-symbol');
+    let clear = document.getElementsByClassName('.clear-symbol');
     let dot = document.getElementsByClassName('dot-symbol');
     let numbers = document.querySelectorAll('.grid-item');
 
@@ -12,5 +12,18 @@
             calculatorInput.value += value;
         })
     });
+
+    equal.addEventListener('click', function (e) {
+        if (display.value === '') {
+            display.value = "";
+        } else {
+            let answer = eval(disply.value);
+            disply.value = answer;
+        }
+    })
+
+    clear.addEventListener('click', function(e){
+        disply.value = "";
+    })
 
 })();
