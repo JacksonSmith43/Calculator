9dce34866b9c506dbc2eaab9629ae3a88eea3f0f
JacksonSmith43 <jessica444@gmx.at>
2023-08-15 11:52:15 +0200

    Input now works.

--- Index.js
+++ Index.js
@@ -1,7 +1,16 @@
-let calculatorInput = document.getElementsByClassName('calculator-form');
-let operation = document.querySelector('.operation-row form input');
-let result = document.getElementsByClassName('equals-symbol');
-let clear = document.getElementsByClassName('clear-symbol');
-let dot = document.getElementsByClassName('dot-symbol');
-let numbers = document.querySelectorAll('#numbers div');
+(function () {
 
+    let calculatorInput = document.querySelector('input[name="display"]');    let operation = document.querySelector('#operation-row');
+    let result = document.getElementsByClassName('equals-symbol');
+    let clear = document.getElementsByClassName('clear-symbol');
+    let dot = document.getElementsByClassName('dot-symbol');
+    let numbers = document.querySelectorAll('.grid-item');
+
+    numbers.forEach(function (button) {
+        button.addEventListener('click', function (e) {
+            let value = e.target.dataset.num;
+            calculatorInput.value += value;
+        })
+    });
+
+})();
